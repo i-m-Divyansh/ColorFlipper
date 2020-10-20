5e7c04a78cdbe557d03a9a163284672a204d3909
i_m.Divyansh <Divyanshbasera36@gmail.com>
2020-10-20 19:57:13 +0530

    Updates

---
+++ precache-manifest.1c2ceadd2f856f1edc362af7cf8d94aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0a854b955a2d99b8044cd9a843c2176",
+    "url": "/colorflipper/index.html"
+  },
+  {
+    "revision": "487bbf3be88e47d10f68",
+    "url": "/colorflipper/static/css/main.1b2256c5.chunk.css"
+  },
+  {
+    "revision": "e7b7f7c5a6e9e7c565cd",
+    "url": "/colorflipper/static/js/2.b00b0982.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/colorflipper/static/js/2.b00b0982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "487bbf3be88e47d10f68",
+    "url": "/colorflipper/static/js/main.69bb17b0.chunk.js"
+  },
+  {
+    "revision": "a2a2393a88d1b2d92eff",
+    "url": "/colorflipper/static/js/runtime-main.a0f89655.js"
+  }
+]);
